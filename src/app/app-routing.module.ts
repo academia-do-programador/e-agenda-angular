2085dbd3fa73592103e4a89380133cbf9980ad31
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-10-18 15:36:38 -0300

    add: route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,33 @@
 import { NgModule, inject } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
+  CanActivateFn,
   ResolveFn,
+  Router,
   RouterModule,
   Routes,
+  UrlTree,
 } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { InserirContatoComponent } from './views/contatos/inserir-contato/inserir-contato.component';
-import { ListarContatosComponent } from './views/contatos/listar-contatos/listar-contatos.component';
-import { EditarContatoComponent } from './views/contatos/editar-contato/editar-contato.component';
-import { ExcluirContatoComponent } from './views/contatos/excluir-contato/excluir-contato.component';
-import { FormsContatoViewModel } from './views/contatos/models/forms-contato.view-model';
-import { ContatosService } from './views/contatos/services/contatos.service';
-import { VisualizarContatoViewModel } from './views/contatos/models/visualizar-contato.view-model';
-import { ListarContatoViewModel } from './views/contatos/models/listar-contato.view-model';
-import { InserirCompromissoComponent } from './views/compromissos/inserir-compromisso/inserir-compromisso.component';
-import { ListarCompromissosComponent } from './views/compromissos/listar-compromissos/listar-compromissos.component';
+import { authGuard } from './core/auth/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [authGuard],
   },
 
   {
     path: 'contatos',
     loadChildren: () =>
       import('./views/contatos/contatos.module').then((m) => m.ContatosModule),
+    canActivate: [authGuard],
   },
 
   {
@@ -40,6 +36,7 @@ const routes: Routes = [
       import('./views/compromissos/compromissos.module').then(
         (m) => m.CompromissosModule
       ),
+    canActivate: [authGuard],
   },
 
   {
@@ -48,18 +45,21 @@ const routes: Routes = [
       import('./views/categorias/categorias.module').then(
         (m) => m.CategoriasModule
       ),
+    canActivate: [authGuard],
   },
 
   {
     path: 'despesas',
     loadChildren: () =>
       import('./views/despesas/despesas.module').then((m) => m.DespesasModule),
+    canActivate: [authGuard],
   },
 
   {
     path: 'tarefas',
     loadChildren: () =>
       import('./views/tarefas/tarefas.module').then((m) => m.TarefasModule),
+    canActivate: [authGuard],
   },
 ];
 
