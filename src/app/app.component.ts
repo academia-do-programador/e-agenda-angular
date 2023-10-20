1ea5de7f91b4a893f0767f1451571b7779883f96
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-10-20 11:41:13 -0300

    add: loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { LoadingService } from './core/loading/services/loading.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'e-agenda';
+export class AppComponent implements OnInit {
+  estaCarregando$?: Observable<boolean>;
+
+  constructor(private router: Router, private loadingService: LoadingService) {
+    this.router.events.subscribe((routerEvent: Event) =>
+      this.atualizarStatusCarregamento(routerEvent)
+    );
+  }
+
+  ngOnInit(): void {
+    this.estaCarregando$ = this.loadingService.estaCarregando;
+  }
+
+  atualizarStatusCarregamento(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loadingService.carregar();
+    } else if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loadingService.parar();
+    }
+  }
 }
