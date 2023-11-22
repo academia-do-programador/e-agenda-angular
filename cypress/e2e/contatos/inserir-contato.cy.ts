f62e20e34be5dee403958b417fa8ab262b698a60
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-11-22 13:32:36 -0300

    add: teste de cadastro de contato

--- cypress/e2e/contatos/inserir-contato.cy.ts
+++ cypress/e2e/contatos/inserir-contato.cy.ts
@@ -1,13 +1,29 @@
 describe('Processo de Cadastro do Contato', () => {
   beforeEach(() => {
     cy.login('tiago@gmail.com', 'Tiago@123');
-  });
 
-  it('Deve entrar na página corretamente', () => {
     cy.contains('a', 'Contatos').click();
 
     cy.get('[data-cy=btnAdicionar]').click();
+  });
 
+  it('Deve entrar na página corretamente', () => {
     cy.url().should('contain', '/contatos/inserir');
+  });
+
+  it('Deve cadastrar o contato corretamente', () => {
+    cy.get('[data-cy=txtNome]').type('Teste Automatizado Cypress');
+
+    cy.get('[data-cy=txtEmail]').type('teste@cypress.com');
+
+    cy.get('[data-cy=txtTelefone]').type('49 99999-0000');
+
+    cy.get('[data-cy=txtCargo]').type('Testador');
+
+    cy.get('[data-cy=txtEmpresa]').type('Academia do Programador');
+
+    cy.get('[data-cy=btnGravar]').click();
+
+    cy.url().should('contain', '/contatos/listar');
   });
 });
